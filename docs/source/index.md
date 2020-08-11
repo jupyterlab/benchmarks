bd3e4376a91c924a3c0da55041f1d3fac5d1aa0c
Eric Charles <eric@datalayer.io>
2020-08-11 07:14:55 +0200

    Initial rtd docs content

---
+++ docs/source/index.md
@@ -0,0 +1,38 @@
+JupyterLab Benchmarks Documentation
+===================================
+
+**Benchmarking tools for JupyterLab**
+
+## Site Contents
+
+```{toctree}
+---
+maxdepth: 2
+caption: Issues
+---
+issues/index
+```
+
+```{toctree}
+---
+maxdepth: 2
+caption: Benchmarks
+---
+benchmarks/index
+```
+
+```{toctree}
+---
+maxdepth: 2
+caption: Profiling
+---
+profiling/index
+```
+
+```{toctree}
+---
+maxdepth: 2
+caption: Fixes
+---
+fixes/index
+```
---
+++ setup.py
@@ -0,0 +1,57 @@
+"""myst-parser package setup."""
+from importlib import import_module
+
+from setuptools import find_packages, setup
+
+setup(
+    name="jupyterlab-benchmarks",
+    version=import_module("jupyterlab_benchmarks").__version__,
+    description=(
+        "Benchmarking tools for JupyterLab."
+    ),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jupyterlab/benchmarks",
+    project_urls={"Documentation": "https://jupyterlab-benchmarks.readthedocs.io"},
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="jupyterlab benchmarks",
+    python_requires=">=3.6",
+    install_requires=[
+    ],
+    extras_require={
+        "sphinx": [],
+        "code_style": [
+            "flake8<3.8.0,>=3.7.0", 
+            "black", 
+            "pre-commit==1.17.0"
+        ],
+        "testing": [
+        ],
+        # Note: This is only required for internal use
+        "rtd": [
+            "myst_parser",
+            "markdown-it-py~=0.4.5",
+            "pyyaml",
+            "docutils>=0.15",
+            "sphinx>=2,<3",
+            "sphinxcontrib-bibtex", 
+            "ipython", 
+            "sphinx-book-theme", 
+            "sphinx_tabs"
+        ],
+    },
+    zip_safe=True,
+)
