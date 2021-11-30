d9a0a18ddfc0cbd712945a0349caf0bd994cbca4
Frédéric Collonval <fcollonval@gmail.com>
2021-11-30 08:38:07 +0100

    Run JupyterLab in dev mode

--- tests/jupyter_server_config.py
+++ tests/jupyter_server_config.py
@@ -7,11 +7,14 @@ if getpass.getuser() == "jovyan":
 
 c.ServerApp.port = 9999
 c.ServerApp.open_browser = False
-c.LabApp.dev_mode = False
 
 c.ServerApp.root_dir = mkdtemp(prefix='benchmarks-test-')
+
 c.ServerApp.token = ""
 c.ServerApp.password = ""
 c.ServerApp.disable_check_xsrf = True
+
+c.LabApp.dev_mode = True
 c.LabApp.expose_app_in_browser = True
+
 c.RetroApp.expose_app_in_browser = True
