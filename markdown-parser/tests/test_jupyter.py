3a39553a774b2adf0d7242055d3de29a6261a1e0
Frédéric Collonval <fcollonval@gmail.com>
2022-04-20 13:35:52 +0200

    Speed tests by using the same web page

--- markdown-parser/tests/test_jupyter.py
+++ markdown-parser/tests/test_jupyter.py
@@ -6,12 +6,7 @@ from .utils import commonmark_gfm_tests, get_jupyterlab_rendered_markdown
 
 
 @pytest.mark.parametrize("gfm", commonmark_gfm_tests())
-def test_nbconvert_jupyterlab(gfm):
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("http://127.0.0.1:9999/")
-        lab_html = page.evaluate(get_jupyterlab_rendered_markdown, gfm["markdown"])
-        print(repr(gfm["markdown"]))
-        assert lab_html == markdown2html(gfm["markdown"])
-        browser.close()
+def test_nbconvert_jupyterlab(jupyterlab_page, gfm):
+    lab_html = jupyterlab_page.evaluate(get_jupyterlab_rendered_markdown, gfm["markdown"])
+    print(repr(gfm["markdown"]))
+    assert lab_html == markdown2html(gfm["markdown"])
