9a5f370aa4e7a48a73e59a388126e4a8c964ed1f
Frédéric Collonval <fcollonval@gmail.com>
2022-06-09 13:31:17 +0200

    Create reusable workflow

--- memory-leaks/tests/notebook.test.mjs
+++ memory-leaks/tests/notebook.test.mjs
@@ -1,13 +1,18 @@
 import { findLeaks } from "fuite";
 import { expect } from "chai";
 import { asyncIterableToArray, formatResult } from "./utils.mjs";
-import * as notebookScenario from './notebook.mjs';
+import * as notebookScenario from "./notebook.mjs";
 
 const URL = process.env.TARGET_URL ?? "http://localhost:9999/lab";
 
 describe("Notebook memory leaks", () => {
   it("Opening a notebook", async () => {
-    const results = await asyncIterableToArray(findLeaks(URL, { scenario: notebookScenario }));
+    const results = await asyncIterableToArray(
+      findLeaks(URL, {
+        iterations: parseInt(process.env.MEMORY_LEAK_NSAMPLES ?? '7', 10),
+        scenario: notebookScenario,
+      })
+    );
 
     console.log(formatResult(results[0]));
 
