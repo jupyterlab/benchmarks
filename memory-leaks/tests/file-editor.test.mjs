a85353a02362b38b9833afe505dcfd7154a8502c
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-07-08 17:16:01 +0200

    More expectation tuning

--- memory-leaks/tests/file-editor.test.mjs
+++ memory-leaks/tests/file-editor.test.mjs
@@ -3,6 +3,6 @@ import * as scenario from "./file-editor.mjs";
 
 describe("# File editor memory leaks", () => {
   it("Opening a text file", async () => {
-    await testScenario(scenario, { expectations: [{ objects: 608 }] });
+    await testScenario(scenario, { expectations: [{ objects: 653 }] });
   });
 });
